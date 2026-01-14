38cd03849804d078ccae37a9b99582e3ee73ea5d
HaEun Noh <okhagrace06@naver.com>
2026-01-15 07:42:12 +0900

    [Bronze V] Title: 직사각형, Time: 164 ms, Memory: 17528 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(a * b);
+    }
+}
