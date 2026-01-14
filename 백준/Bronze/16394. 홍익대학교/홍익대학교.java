777d82ca6e1a0f8cd942201c1ec3e73d431aecf5
HaEun Noh <okhagrace06@naver.com>
2026-01-15 07:46:26 +0900

    [Bronze V] Title: 홍익대학교, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        System.out.println(year-1946);
+    }
+}
