840005dd0cfafa0c23a2f3227df58f7e4e896523
HaEun Noh <okhagrace06@naver.com>
2026-01-19 07:41:28 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long num1 = scan.nextLong();
+        long num2 = scan.nextLong();
+        long num3 = scan.nextLong();
+        
+
+        System.out.println(num1+num2+num3);
+    }
+}
