1580fb13d78cf86971bebf0365570f5ac63ed0fa
HaEun Noh <okhagrace06@naver.com>
2026-01-18 17:23:43 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13880 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,4 +1,8 @@
-public class Main {
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
