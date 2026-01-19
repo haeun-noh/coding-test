e9a84f338311fc0430b6dde07ee39a55c5dc829b
HaEun Noh <okhagrace06@naver.com>
2026-01-20 07:51:56 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 708 ms, Memory: 236836 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int len = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < len; i++ ) {
+            st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            bw.write((num1+num2)+"\n");
+        }
+        
+        bw.close();
+    }
+}
