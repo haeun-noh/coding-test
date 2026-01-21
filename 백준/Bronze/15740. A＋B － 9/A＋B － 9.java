3e622d33a39ffa15c3184c4d7c60fea7db27aaa8
HaEun Noh <okhagrace06@naver.com>
2026-01-22 07:48:28 +0900

    [Bronze V] Title: A+B - 9, Time: 172 ms, Memory: 17772 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long num1 = scan.nextInt();
+        long num2 = scan.nextInt();
+
+        System.out.println(num1+num2);
+    }
+}
