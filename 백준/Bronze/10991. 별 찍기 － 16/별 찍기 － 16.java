1ae42b607536267c23ce0772e1798b9bdc9e03d0
HaEun Noh <okhagrace06@naver.com>
2026-01-23 08:05:00 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 308 ms, Memory: 20424 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++ ) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
