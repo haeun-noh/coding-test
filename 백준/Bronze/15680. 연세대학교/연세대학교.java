e341ee6a76b36aca4ee24db9d7ece19c954a3908
HaEun Noh <okhagrace06@naver.com>
2026-01-30 23:09:20 +0900

    [Bronze V] Title: 연세대학교, Time: 168 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = Integer.parseInt(scan.nextLine());
+
+        switch(num) {
+            case 0: 
+                System.out.println("YONSEI");
+                break;
+            case 1:
+                System.out.println("Leading the Way to the Future");
+                break;
+        }
+    }
+}
