1d7838564f1bdcf557b0d9d43d1f8f04491eccec
HaEun Noh <okhagrace06@naver.com>
2026-02-04 07:46:09 +0900

    [Bronze V] Title: WARBOY, Time: 152 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println((3 * (b/a)) * c);
+    }
+}
