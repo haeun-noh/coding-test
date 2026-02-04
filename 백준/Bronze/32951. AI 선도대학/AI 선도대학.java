bb8f908fef61dbe7bb7dc98cde5a689ea524bfeb
HaEun Noh <okhagrace06@naver.com>
2026-02-05 07:45:42 +0900

    [Bronze V] Title: AI 선도대학, Time: 168 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextInt() - 2024);
+    }
+}
