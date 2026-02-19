1510be04ea4e69b84eddb92ec6f61f13cb058282
HaEun Noh <okhagrace06@naver.com>
2026-02-20 07:43:59 +0900

    [Bronze III] Title: A+B - 6, Time: 164 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int len = Integer.parseInt(scan.nextLine());
+
+        for ( int i = 0; i < len; i++ ) {
+            int[] arr = Arrays.stream(scan.nextLine().split(","))
+                            .mapToInt(Integer::parseInt)
+                            .toArray();
+
+            System.out.println(arr[0] + arr[1]);
+        }
+    }
+}
