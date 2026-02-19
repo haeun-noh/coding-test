0b678333ae4a259b4457468b589d7da058504dd3
HaEun Noh <okhagrace06@naver.com>
2026-02-20 07:41:04 +0900

    [Bronze V] Title: A+B - 2, Time: 148 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+
+        System.out.println(n1+n2);
+    }
+}
