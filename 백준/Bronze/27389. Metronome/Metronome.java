3033194fac868ad90b1e9967f2fe888b01e25096
HaEun Noh <okhagrace06@naver.com>
2026-03-08 23:59:09 +0900

    [Bronze V] Title: Metronome, Time: 180 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double input = scan.nextDouble();
+
+        System.out.println(input / 4);
+    }
+}
